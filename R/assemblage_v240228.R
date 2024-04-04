3be7dd1524f0ef7fdf32f7ca4deedb186c9a3230
ChristopheBarrette <christophebarrette@gmail.com>
2024-04-04 11:04:58 -0400

    Assemblage Regression

--- R/assemblage_v240228.R
+++ R/assemblage_v240228.R
@@ -780,7 +780,7 @@ nonneg.ridge.meanD = function( y.in, x.in, standardize.values, lambda.grid.C=c()
   # --- Set the Problem
   prob = Problem(loss,constr)
   # --- Solve the Problem
-  sol = CVXR::solve(prob,solver = 'ECOS')
+  sol = solve(prob,solver = 'ECOS')
   # --- Get the betas
   #beta = sol$getValue(coeffs)
   beta=1:10
